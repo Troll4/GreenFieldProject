198747a246b52f032973c1164f73b924c0612c44
AymanGhaith <ayman.ghaith88@gmail.com>
2018-05-02 20:11:16 +0300

    testing the styling of the map

--- react-client/src/components/jobsForUser.jsx
+++ react-client/src/components/jobsForUser.jsx
@@ -12,62 +12,60 @@ class JobsForUser extends React.Component {
 render() {
 
   return (
-  <div className = 'container'>
-    <div className = 'col-1'>
+    <div>
     <div className="jobsDiv"><br />
-      <Link to = {`/UserJobs/${ this.props.item.jobTitle }/${ this.props.item.user }`} activeClassName = "is-active" >
-        <div className = 'row' id="userjob">
-          <div className = 'col-9'>
-            <span><b>Name : </b></span>
-            <span>{this.props.item.user}</span>
-          </div>
-          <div className = 'col-4'>
-            <span><b>Job Title : </b></span>
-            <span>{this.props.item.jobTitle}</span>
-          </div>
-          <div className = 'col-4'>
-            <span><b>Job Category : </b></span>
-            <span>{this.props.item.category}</span>
-          </div>
-        </div><br />
+    <Link to = {`/UserJobs/${ this.props.item.jobTitle }/${ this.props.item.user }`} activeClassName = "is-active" >
+      <Row id="userjob">
+      <Col md={4}>
+      <span><b>Name : </b></span>
+      <span>{this.props.item.user}</span>
+      </Col>
+      <Col md={4}>
+      <span><b>Job Title : </b></span>
+      <span>{this.props.item.jobTitle}</span>
+      </Col>
+      <Col md={4}>
+      <span><b>Job Category : </b></span>
+      <span>{this.props.item.category}</span>
+      </Col>
+    </Row><br />
 
-        <div id="userjob">
-          <div className = 'col-4'>
+        <Row id="userjob">
+            <Col md={4}>
             <span><b>From : </b></span>
-            <span>{this.props.item.from}</span>
-          </div>
-          <div className = 'col-4'>
-            <span><b>To : </b></span>
-            <span>{this.props.item.to}</span>
-          </div>
-        </div><br />
+      <span>{this.props.item.from}</span>
+      </Col>
+      <Col md={4}>
+      <span><b>To : </b></span>
+      <span>{this.props.item.to}</span>
+      </Col>
+    </Row><br />
 
-        <div id="userjob">
-          <div className='col-1'>
-          </div>
-          <div id="description" className = 'col-10'>
-            <span><b>Description : </b></span>
-            <span>{this.props.item.jobDescription}</span>
-          </div>
-          <div className = 'col-1'>
-          </div>
-        </div><br />
+    <Row id="userjob">
+    <Col md={1}>
+    </Col>
+      <Col id="description" md={10}>
+      <span><b>Description : </b></span>
+      <span>{this.props.item.jobDescription}</span>
+      </Col>
+      <Col md={1}>
+      </Col>
+    </Row><br />
 
-        <div>
-          <div className = 'col-8'>
-          </div>
-          <div id='postTime' className = 'col-4'>
-            <span><b>Posted at : </b></span>
-            <span>{this.props.item.created_at.slice(0, 10)}</span>
-          </div>
-        </div>
+     <Row>
+     <Col md={8}>
+      </Col>
+     <Col id='postTime' md={4}>
+      <span><b>Posted at : </b></span>
+      <span>{this.props.item.created_at.slice(0, 10)}</span>
+      </Col>
+     </Row>
 
 
 
-      </Link>
+    </Link>
     </div>
-  </div>
-  </div>
+    </div>
 
     )
   }
